5692d17defc10c38203fbbf25dc468f07889c834
s-amano <amahaya0831@gmail.com>
2021-05-05 17:31:05 +0900

    自分の日記一覧をカードコンポーネントへ変更

--- React/unknwon-react-diary/src/component/FetchMyDiaries.js
+++ React/unknwon-react-diary/src/component/FetchMyDiaries.js
@@ -1,19 +1,35 @@
 import React, { useContext, useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import { Auth, API } from 'aws-amplify';
 import { ApiContext } from '../context/ApiContext';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import Grid from '@material-ui/core/Grid';
 import IconButton from '@material-ui/core/IconButton';
-import DetailsIcon from '@material-ui/icons/Details';
-import { Grid } from '@material-ui/core';
-import Button from '@material-ui/core/Button';
-import { Link } from 'react-router-dom';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
 import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
 import Container from '@material-ui/core/Container';
+import ChromeReaderModeIcon from '@material-ui/icons/ChromeReaderMode';
+
+const useStyles = makeStyles((theme) => ({
+  card: {
+    height: '100%',
+    width: '100%',
+    display: 'flex',
+    marginBottom: '4%',
+    border: 'thick double #32a1ce',
+  },
+  cardContent: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexGrow: 1,
+  },
+}));
 
 const FetchMyDiaries = () => {
+  const classes = useStyles();
   const { setMyDiaryDetail } = useContext(ApiContext);
   const [myDiaries, setMyDiaries] = useState([]);
 
@@ -61,36 +77,38 @@ const FetchMyDiaries = () => {
       <Typography style={{ marginTop: '30px', color: 'black', marginBottom: '10%' }} variant="h6">
         貴方の日記
       </Typography>
-      <List>
-        {myDiaries.map((value) => {
-          const diary = {};
-          const diaryContent = value.content;
-          const diaryReaction = value.reaction;
-          const maxLength = 22;
-          let modifiedDiaryContent = '';
-          if (diaryContent.length > maxLength) {
-            modifiedDiaryContent = diaryContent.substr(0, maxLength) + '...';
-          } else {
-            modifiedDiaryContent = diaryContent;
-          }
+      {myDiaries.map((value) => {
+        const diary = {};
+        const diaryContent = value.content;
+        const diaryReaction = value.reaction;
+        const maxLength = 22;
+        let modifiedDiaryContent = '';
+        if (diaryContent.length > maxLength) {
+          modifiedDiaryContent = diaryContent.substr(0, maxLength) + '...';
+        } else {
+          modifiedDiaryContent = diaryContent;
+        }
 
-          diary.diaryContent = diaryContent;
-          diary.diaryReaction = diaryReaction;
+        diary.diaryContent = diaryContent;
+        diary.diaryReaction = diaryReaction;
 
-          return (
-            <ListItem key={value.id} role={undefined} dense button>
-              <ListItemText style={{ color: 'black', textOverflow: 'ellipsis' }}>{modifiedDiaryContent}</ListItemText>
-              <ListItemSecondaryAction>
+        return (
+          <Grid container>
+            <Card className={classes.card}>
+              <CardContent className={classes.cardContent}>
+                <Typography>{modifiedDiaryContent}</Typography>
+              </CardContent>
+              <CardActions>
                 <Link to="/mydiary-detail" onClick={() => DetailMyDiary(diary)}>
                   <IconButton edge="end" aria-label="detail">
-                    <DetailsIcon />
+                    <ChromeReaderModeIcon />
                   </IconButton>
                 </Link>
-              </ListItemSecondaryAction>
-            </ListItem>
-          );
-        })}
-      </List>
+              </CardActions>
+            </Card>
+          </Grid>
+        );
+      })}
     </Container>
   );
 };
