afcc3413d4ba3e066af0b34b548c3e98cf31b233
{s-amano} <{amahaya0831@gmail.com}>
2021-05-05 17:31:05 +0900

    自分の日記一覧をカードコンポーネントへ変更

--- React/unknwon-react-diary/src/component/MyDiaryDetail.js
+++ React/unknwon-react-diary/src/component/MyDiaryDetail.js
@@ -5,7 +5,6 @@ import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import { Grid } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
-import { Link } from 'react-router-dom';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 
 const MyDiaryDetail = () => {
