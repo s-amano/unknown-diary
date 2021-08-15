fa509ed68e89038ba59433b7ada974b1377f3258
s-amano <amahaya0831@gmail.com>
2021-08-15 18:16:03 +0900

    パッケージ依存の問題を解決

--- React/unknwon-react-diary/src/index.js
+++ React/unknwon-react-diary/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Amplify from '@aws-amplify/core';
+import { Amplify } from 'aws-amplify';
 
 const env = process.env.REACT_APP_ENVIROMENT;
 
