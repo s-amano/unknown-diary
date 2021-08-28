0cad497c9f4509621d52a5d5ceece96cb12e4d27
s-amano <amahaya0831@gmail.com>
2021-08-29 06:53:40 +0900

    pagination front

--- React/unknwon-react-diary/src/component/FetchMyDiaries.js
+++ React/unknwon-react-diary/src/component/FetchMyDiaries.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React, { useState } from 'react';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
@@ -8,6 +8,7 @@ import Typography from '@material-ui/core/Typography';
 import { Link } from 'react-router-dom';
 import Container from '@material-ui/core/Container';
 import FavoriteIcon from '@material-ui/icons/Favorite';
+import MuiPagination from '@material-ui/lab/Pagination';
 
 const useStyles = makeStyles((theme) => ({
   cardContainer: {
@@ -30,49 +31,60 @@ const useStyles = makeStyles((theme) => ({
 
 const FetchMyDiaries = (props) => {
   const classes = useStyles();
+  const [page, setPage] = useState(1);
+
+  const Pagination = withStyles({
+    root: {
+      display: 'inline-block',
+      marginBottom: '10%',
+    },
+  })(MuiPagination);
 
   return (
-    <Container className={classes.cardContainer} maxWidth="md">
-      {props.myDiaries.map((value, key) => {
-        const diaryContent = value.content;
-        const diaryReaction = value.reaction;
-        const diaryTitle = value.title ? value.title : 'タイトルなし';
-        const diaryDate = value.date ? value.date : '日付なし';
-        const maxLength = 37;
-        let modifiedDiaryContent = '';
-        if (diaryContent.length > maxLength) {
-          modifiedDiaryContent = diaryContent.substr(0, maxLength) + '...';
-        } else {
-          modifiedDiaryContent = diaryContent;
-        }
+    <>
+      <Container className={classes.cardContainer} maxWidth="md">
+        {props.myDiaries.map((value, key) => {
+          const diaryContent = value.content;
+          const diaryReaction = value.reaction;
+          const diaryTitle = value.title ? value.title : 'タイトルなし';
+          const diaryDate = value.date ? value.date : '日付なし';
+          const maxLength = 37;
+          let modifiedDiaryContent = '';
+          if (diaryContent.length > maxLength) {
+            modifiedDiaryContent = diaryContent.substr(0, maxLength) + '...';
+          } else {
+            modifiedDiaryContent = diaryContent;
+          }
 
-        return (
-          <Card className={classes.card} key={key}>
-            <Link
-              to={{ pathname: '/mydiary-detail', search: `?id=${value.id}` }}
-              // onClick={() => fetchMyDiary(value.id)
-              style={{ textDecoration: 'none' }}
-            >
-              <CardActionArea>
-                <CardContent className={classes.cardContent}>
-                  <Typography style={{ textAlign: 'left' }} gutterBottom variant="h5" component="h2">
-                    {diaryTitle}
-                  </Typography>
-                  <Typography style={{ textAlign: 'left' }} variant="body2" color="textSecondary" component="p">
-                    {modifiedDiaryContent}
-                  </Typography>
-                </CardContent>
-                <CardActions style={{ paddingTop: '0px' }}>
-                  <FavoriteIcon style={{ marginRight: '2%' }} color="error" />
-                  <p style={{ margin: 0, fontWeight: 'bold', fontSize: '16px' }}>{diaryReaction}</p>
-                  <Typography style={{ marginLeft: 'auto' }}>{diaryDate}</Typography>
-                </CardActions>
-              </CardActionArea>
-            </Link>
-          </Card>
-        );
-      })}
-    </Container>
+          return (
+            <Card className={classes.card} key={key}>
+              <Link
+                to={{ pathname: '/mydiary-detail', search: `?id=${value.id}` }}
+                // onClick={() => fetchMyDiary(value.id)
+                style={{ textDecoration: 'none' }}
+              >
+                <CardActionArea>
+                  <CardContent className={classes.cardContent}>
+                    <Typography style={{ textAlign: 'left' }} gutterBottom variant="h5" component="h2">
+                      {diaryTitle}
+                    </Typography>
+                    <Typography style={{ textAlign: 'left' }} variant="body2" color="textSecondary" component="p">
+                      {modifiedDiaryContent}
+                    </Typography>
+                  </CardContent>
+                  <CardActions style={{ paddingTop: '0px' }}>
+                    <FavoriteIcon style={{ marginRight: '2%' }} color="error" />
+                    <p style={{ margin: 0, fontWeight: 'bold', fontSize: '16px' }}>{diaryReaction}</p>
+                    <Typography style={{ marginLeft: 'auto' }}>{diaryDate}</Typography>
+                  </CardActions>
+                </CardActionArea>
+              </Link>
+            </Card>
+          );
+        })}
+      </Container>
+      <Pagination count={10} color="primary" onChange={(e, page) => setPage(page)} page={page} />
+    </>
   );
 };
 
