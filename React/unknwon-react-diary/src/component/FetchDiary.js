f2f6c759e097b7985ac89a0a405167ba20d95ad6
s-amano <amahaya0831@gmail.com>
2021-09-17 01:30:35 +0900

    comment padding

--- React/unknwon-react-diary/src/component/FetchDiary.js
+++ React/unknwon-react-diary/src/component/FetchDiary.js
@@ -219,7 +219,7 @@ const DiaryFetch = () => {
 
         {diary.comments ? (
           diary.comments.map((comment, index) => (
-            <div key={index} className="bg-white shadow-xl rounded-2xl mb-4 sm:w-9/12 md:w-2/3 text-left">
+            <div key={index} className="bg-white shadow-xl rounded-2xl mb-4 sm:w-9/12 md:w-2/3 text-left py-1 px-2">
               <p className="ml-3">{comment}</p>
             </div>
           ))
